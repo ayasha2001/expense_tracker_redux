6924bd066fbb36b08f245b2302d329c115877af4
ayasha2001 <c.ayasha.pandey@generalaeronautics.com>
2024-01-27 23:11:58 +0530

    tdd asyn code

--- src/components/auth/LoginSignup.test.js
+++ src/components/auth/LoginSignup.test.js
@@ -1,3 +1,7 @@
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
 test("should render the component", () => {
   render(
     <Provider store={store}>
