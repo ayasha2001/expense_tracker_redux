e363a22f8acca41e52dfb30957ef7114237ab1c7
ayasha2001 <c.ayasha.pandey@generalaeronautics.com>
2024-01-30 13:55:45 +0530

    test cases added

---
+++ src/pages/ExpensePage.test.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Provider } from "react-redux";
+import ExpensePage from "./ExpensePage";
+import store from "../store/store";
+
+describe("ExpensePage Component", () => {
+  test("should fetch expenses on component mount", async () => {
+    // Mock the global fetch function
+    global.fetch = jest.fn().mockResolvedValue({
+      ok: true,
+      json: () =>
+        Promise.resolve({
+          amount: "500",
+          date: "2024-01-26",
+          description: "on way back",
+          type: "Petrol",
+          id: "-Np3e1S4R6VzMojZ65yY",
+        }),
+    });
+
+    render(
+      <Provider store={store}>
+        <ExpensePage />
+      </Provider>
+    );
+
+    // to ensure fetch function is called with the correct URL
+    expect(global.fetch).toHaveBeenCalledWith(
+      "https://react-ecom-bootstrap-default-rtdb.asia-southeast1.firebasedatabase.app/expenses.json"
+    );
+
+    // Wait for the component to render
+    waitFor(() => {
+      const text = screen.getByText("Petrol");
+      expect(text).toBeInTheDocument();
+    });
+
+    // Clean up mock
+    global.fetch.mockRestore();
+  });
+
+});
